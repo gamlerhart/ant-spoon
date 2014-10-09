62eee4ea51449223d8f52ef8b876612930929fe3
Roman Stoffel <roman.stoffel@gamlor.info>
2014-10-09 23:10:19 +0900

    Fix ant test

--- test/test-sequence-in-container.bat
+++ test/test-sequence-in-container.bat
@@ -1,22 +1,14 @@
 echo "Testing Ant Now"
 
-IF NOT DEFINED RUBY_HOME (
-	echo "%%RUBY_HOME%% is missing"
+IF NOT DEFINED ANT_HOME (
+	echo "%%ANT_HOME%% is missing"
 	exit 1
 )
 
-IF NOT EXIST %RUBY_HOME%\bin\ruby.exe(
-	echo "%RUBY_HOME%\bin\ruby.exe didn't exit"
+IF NOT EXIST %ANT_HOME%\bin\ant.bat (
+	echo "%%ANT_HOME%%\bin\ant.bat"
 	exit 2
 )
 
-gem install haml
-IF %ERRORLEVEL% NEQ 0 (
-	echo "gem command doesn't work"
-	exit %ERRORLEVEL%
-)
-ruby hello-world.rb
-IF %ERRORLEVEL% NEQ 0 (
-	echo "ruby command doesn't work"
-	exit %ERRORLEVEL%
-)
+ant -f hello-ant.xml
+IF %ERRORLEVEL% NEQ 0 exit %ERRORLEVEL%
